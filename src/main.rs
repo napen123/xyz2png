9dbff1f822e1419db173d45620876a89dac3c8e2
Napen123 <napen123@gmail.com>
2019-06-08 21:29:05 -0600

    Add license text to stb.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2018-2019, Ethan Dagner.
+Copyright (c) 2018-2019 Ethan Dagner.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
